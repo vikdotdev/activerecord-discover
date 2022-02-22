e3c5b278acadd5e0bb0d4ce3447c65fd989af139
Viktor Habchak <vikdotdev@gmail.com>
2022-02-22 20:44:45 +0200

    fix print_callbacks method polluting console

--- lib/activerecord-discover/console_methods.rb
+++ lib/activerecord-discover/console_methods.rb
@@ -1,33 +1,35 @@
-def print_callbacks(model, kind: nil, name: nil)
-  unless model.ancestors.include?(ActiveRecord::Base)
-    puts "\"#{model}\" is not an ActiveRecord::Base ancestor."
-    return
+module ActiveRecordDiscover
+  class PrintWrapper
+    def self.print(model, kind: nil, name: nil)
+      unless model.ancestors.include?(ActiveRecord::Base)
+        puts "\"#{model}\" is not an ActiveRecord::Base ancestor."
+        return
+      end
+
+      ActiveRecordDiscover::Printer.print(
+        ActiveRecordDiscover::CallbackList.filter(model, kind: kind, name: name)
+      )
+
+      nil
+    end
   end
 
-  ActiveRecordDiscover::Printer.print(
-    ActiveRecordDiscover::CallbackList.filter(model, kind: kind, name: name)
-  )
-
-  nil
-end
-
-module ActiveRecordDiscover
   module ConsoleMethods
     Permutator.callback_pairs.map do |kind, name|
       define_method("discover_#{kind}_#{name}_callbacks_of") do |model|
-        print_callbacks(model, kind: kind, name: name)
+        PrintWrapper.print(model, kind: kind, name: name)
       end
     end
 
     Permutator.callback_kinds.map do |kind|
       define_method("discover_#{kind}_callbacks_of") do |model|
-        print_callbacks(model, kind: kind)
+        PrintWrapper.print(model, kind: kind)
       end
     end
 
     Permutator.callback_names.map do |name|
       define_method("discover_#{name}_callbacks_of") do |model|
-        print_callbacks(model, name: name)
+        PrintWrapper.print(model, name: name)
       end
     end
 

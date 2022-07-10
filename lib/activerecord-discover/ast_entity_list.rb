1e4a8fddc22628263825881dd6d8f66e4d0b62de
Viktor Habchak <vikdotdev@gmail.com>
2022-07-10 16:19:58 +0300

    remove PrinterWrapper, move functionality to Printer

--- lib/activerecord-discover/ast_entity_list.rb
+++ lib/activerecord-discover/ast_entity_list.rb
@@ -16,11 +16,6 @@ module ActiveRecordDiscover
       @entities = []
     end
 
-    # move to some helper or even method in printer
-    def not_found_message
-      "No #{self.class.to_s.split('::').last.to_s.delete_prefix('AST').delete_suffix('List')} found"
-    end
-
     def each(&block)
       @entities.each(&block)
       self
--- spec/console_methods_spec.rb
+++ spec/console_methods_spec.rb
@@ -5,9 +5,40 @@ RSpec.describe ConsoleMethods do
     include ConsoleMethods
   end
 
-  describe 'console methods' do
-    let(:dummy_console_env) { DummyConsoleEnvironment.new }
+  let(:dummy_console_env) { DummyConsoleEnvironment.new }
 
+  describe 'outputs when entity list is empty' do
+    describe "when constant is not model" do
+      it 'prints that class is not an ActiveRecord::Base ancestor' do
+        assert_output(/is not an ActiveRecord::Base ancestor/) do
+          dummy_console_env.discover_callbacks_of(dummy_klass)
+        end
+
+        assert_output(/is not an ActiveRecord::Base ancestor/) do
+          dummy_console_env.discover_validations_of(dummy_klass)
+        end
+      end
+    end
+
+    describe "when constant is a model" do
+      context 'when callbacks' do
+        it 'prints that no callbacks were found' do
+          assert_output(/No callbacks found/) do
+            dummy_console_env.discover_callbacks_of(dummy_model)
+          end
+        end
+      end
+      context 'when validations' do
+        it 'prints that no validations were found' do
+          assert_output(/No validations found/) do
+            dummy_console_env.discover_validations_of(dummy_model)
+          end
+        end
+      end
+    end
+  end
+
+  describe 'console methods' do
     PermutationHelper.callback_pairs.each do |kind, name|
       it "responds with #{kind} and #{name}" do
         assert_respond_to dummy_console_env, "discover_#{kind}_#{name}_callbacks_of"
